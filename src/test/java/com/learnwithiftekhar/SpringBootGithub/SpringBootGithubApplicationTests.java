0a0ffccc3ad3b3245cff27e929312f982da95a5b
Iftekhar Hossain <learnwithiftekhar@gmail.com>
2024-02-20 22:28:40 +0600

    initial commit

---
+++ src/test/java/com/learnwithiftekhar/SpringBootGithub/SpringBootGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnwithiftekhar.SpringBootGithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
